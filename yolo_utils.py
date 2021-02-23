089ca14a0f5da11f684f9c587800293b4ee84c49
mstryjek <73649466+mstryjek@users.noreply.github.com>
2021-02-23 20:58:33 +0100

    + opencv models loading and handling

--- yolo_utils.py
+++ yolo_utils.py
@@ -5,75 +5,52 @@ import numpy as np
 import cv2
 
 
-def get_net(yconfig='YOLO_object_det/darknet/cfg/yolov4-custom.cfg', yweights='mydrive/yolov4/backup/yolov4-obj_last.weights'):
+def get_net_layers(yconfig='YOLO_object_det/darknet/cfg/yolov3-custom.cfg', yweights='mydrive/yolov4/backup/yolov4-obj_last.weights'):
     """
     Gets cv2.dnn.Net from darknet driectory
     """
-    return cv2.dnn.readNetFromDarknet(yconfig, yweights)
+    net = cv2.dnn.readNetFromDarknet(yconfig, yweights)
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    return net, output_layers
 
 
-
-def get_predictions(net, img):
+def get_predictions(net, img, out_layers):
     """
     Gets raw predictions from YOLO with image.
     """
     if isinstance(img, str):
         img = cv2.imread(img)
+    cv2.resize(img, (416, 416))
+    blob = cv2.dnn.blobFromImage(img)
     net.setInput(img)
-    return net.forward()
+    return net.forward(out_layers)
 
 
-
-
-def get_labels(predictions, imgshape=(416, 416, 3), with_classes=True, format_corners=False):
+def get_labels(predictions, thresh):
     """
-    Label extraction from YOLO model predictions. Transforms a grid of YOLO labels into array of
-    coordinates (and classes). \\
-    Params: \\
-    -predictions    -> raw output from YOLO detector \\
-    -imgshape       -> shape of images fed into YOLO \\
-    -with_classes   -> whether to include object class indices in output \\
-    -fromat_corners -> whether to return labels as top-left and bottom-right corners instead of
-    middle, width, height \\
-    Returns: \\
-    If not format_corners: \\
-    -Array of shape (num_objects, 4) where 2 is (x_middle, y_middle, h, w) if not with_classes, otherwise
-    (num_objects, 5) where 3 is (x_middle, y_middle, h, w, class_index)
-    Otherwise: \\
-    -Same as above, except first 4 variables are (x_top, y_left, x_bottom, y_right)
+    Extracting labels from raw YOLO outputs.
     """
-    predictions = np.squeeze(predictions) ## redundant dimension reduction
-    ## Y is the vertical dimension of the image, X is the horizontal
-    gridtile_y = imgshape[0] // predictions.shape[0] ## Single grid tile size
-    gridtile_x = imgshape[1] // predictions.shape[1]
-
-    labels = [] ## Final outputs
-    classes = []
-    for x in range(len(predictions)):
-        for y in range(len(predictions[x])):
-            hasobject, (ym, xm, h, w) = predictions[x, y, :5]
-            if hasobject:                    
-                y_middle = int(y*gridtile_y + ym*gridtile_y)  ## Grid tile local to
-                x_middle = int(x*gridtile_x + xm*gridtile_x)  ## image global coords
-                height = int(gridtile_y*w)
-                width = int(gridtile_x*h)
-                if not format_corners:
-                    labels.append([y_middle, x_middle, height, width])
-                else:
-                    y_top = y_middle - (height//2) ## Top right corner
-                    x_left = x_middle - (width//2)
-                    y_bottom = y_middle + (height//2) ## Bottom left corner
-                    x_right = y_middle + (width//2)
-                    labels.append([y_top, x_left, y_bottom, x_right])
-                if with_classes:
-                    classes.append(np.argmax(labels[x, y, 5:]))
-
-    if with_classes:
-        return np.concatenate((labels, classes), axis=-1)
-    return np.array(labels)
+    class_ids = []
+    confidences = []
+    boxes = []
+    width, height = (32, 32)     ## 416/13
+    for out in predictions:      ## 3
+        for detection in out:    ## 509, 2018, 8112
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > thresh:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+    return confidences, class_ids, boxes
     
-
-
-
-
-
